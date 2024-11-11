a6142e4218428fb108d2bacba83531cf371354a4
woony-kim <woony.kim@balancehero.com>
2024-11-11 23:47:14 +0530

    initial commit

---
+++ src/main/java/org/example/nextjsspringbootkakaooauthintegration/NextjsSpringbootKakaoOauthIntegrationApplication.java
@@ -0,0 +1,13 @@
+package org.example.nextjsspringbootkakaooauthintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextjsSpringbootKakaoOauthIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextjsSpringbootKakaoOauthIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/nextjsspringbootkakaooauthintegration/NextjsSpringbootKakaoOauthIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nextjsspringbootkakaooauthintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextjsSpringbootKakaoOauthIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
